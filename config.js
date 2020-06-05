0b00f628a162ec03ecb0be4212fd62fbb44efc4f
John Corwin <jcorwin@zixcorp.com>
2020-06-05 14:10:10 -0500

    add twitter functionality

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: 'B9n6MyQXb5rsAgzymShRs3VHF',
+  consumer_secret: 'SVaoYWIkD5qGnbollOZLBXcD8klkJU0sIdWO4k2J7cSfK70uhc',
+  access_token_key: '1025348358-xLoZl3KxI91jNfibSK6qj0F04Ih1KXupGe39C8I',
+  access_token_secret: 'hvyghsrkEqRPRYJO6rt525EzXXsIsbzE2GmGXD3gHf5aa'
+}
